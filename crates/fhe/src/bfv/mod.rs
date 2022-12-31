c9422a6b9e3bd4ad1d8877600ce8dd4a689bb9c4
Janmajaya Mall <janmajaya.choithram@gmail.com>
2022-12-31 16:14:26 +0530

    fix bfv mod export

--- crates/fhe/src/bfv/mod.rs
+++ crates/fhe/src/bfv/mod.rs
@@ -15,15 +15,9 @@ mod rgsw_ciphertext;
 pub mod traits;
 pub use ciphertext::Ciphertext;
 pub use encoding::Encoding;
-pub use keys::{
-	EvaluationKey, EvaluationKeyBuilder, GaloisKey, PublicKey, RelinearizationKey, SecretKey,
-};
+pub use keys::{EvaluationKey, EvaluationKeyBuilder, PublicKey, RelinearizationKey, SecretKey};
 pub use ops::{dot_product_scalar, Multiplicator};
 pub use parameters::{BfvParameters, BfvParametersBuilder};
 pub use plaintext::Plaintext;
 pub use plaintext_vec::PlaintextVec;
-pub use proto::bfv::{
-	EvaluationKey as EvaluationKeyProto, GaloisKey as GaloisKeyProto, PublicKey as PublicKeyProto,
-	RelinearizationKey as RelinearizationKeyProto,
-};
 pub use rgsw_ciphertext::RGSWCiphertext;
